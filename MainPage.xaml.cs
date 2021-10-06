6e08a8f7d849a9767c5c33f45e5e357fe8097699
BrunaGross <55264314+BrunaGross@users.noreply.github.com>
2021-10-06 11:26:24 -0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TP01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void onClickOk(object sender, EventArgs args)
+        {
+            print("[INFO] Ok\n");
+            if (user.Text.Contains("@") && user.Text.Contains(".com"))
+            {
+                print("[INFO]" + user.Text + " logado com sucesso!");
+            }
+            else
+            {
+                print("[ERRO] identificador inválido\n");
+            }
+        }
+        void onClickLimpar(object sender, EventArgs args)
+        {
+            print("[INFO] Limpar\n");
+            user.Text = "";
+            pass.Text = "";
+            consoleLog.Text = "";
+        }
+        void onClickCreditos(object sender, EventArgs args)
+        {
+            print("[INFO] Creditos\n");
+            print("Criado por: Bruna Gross CB3001695 e Gabriel Costa CB3001717\n");
+        }
+
+        void print(String message)
+        {
+            consoleLog.Text += message;
+        }
+    }
+}
